d7c850a75e3304ef6741b61cfdedbb2a57e5f7d8
krishnaanaril <krishnamohan.a.m@gmail.com>
2022-12-17 12:35:21 +0530

    feat: add basic blog structure

---
+++ components/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+    return (
+        <div>
+            <h2>Posts</h2>
+            <div>{children}</div>
+        </div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+
+export default function Home({ allPostsData }) {
   return (
     <>
       <h1 className={styles.title}>
         Read <Link href="/posts/first-post">this page!</Link>
       </h1>
       <h3>Remove base path</h3>
+      <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
     </>
   );
 }
