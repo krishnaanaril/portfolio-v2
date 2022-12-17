29359b5873f553da79ce7fa898ae293860a1c37b
krishnaanaril <krishnamohan.a.m@gmail.com>
2022-12-17 22:18:14 +0530

    feat: add footer and tailwind config

---
+++ components/footer.tsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+    return (
+        <footer className="flex flex-col bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-50">
+            <div className="mt-10">
+                <ul className="flex flex-row justify-between max-w-sm mx-auto pl-4 pr-4">
+                    <li className="hover:text-gray-600 dark:hover:text-gray-100">
+                        <a href="https://twitter.com/KrishnaAnaril">Twitter</a>
+                    </li>
+                    <li>
+                        <a href="https://krishnamohan.dev/sitemap-blog.xml">Sitemap</a>
+                    </li>
+                    <li>
+                        <a href="https://krishnamohan.dev/feed.xml">RSS</a>
+                    </li>
+                    <li>
+                        <a href="mailto:krishnamohan.a.m@gmail.com">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="mx-auto p-5 text-sm">
+                <p>© 2021-present <a className="font-bold" href="https://krishnamohan.dev/">Krishna Mohan A M</a>. All Rights
+                    Reserved.</p>
+            </div>
+        </footer>
+    );
+}
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,11 @@
+import Footer from './footer';
+
 export default function Layout({ children }) {
     return (
         <div>
             <h2>Posts</h2>
             <div>{children}</div>
+            <Footer></Footer>
         </div>
     );
 }
