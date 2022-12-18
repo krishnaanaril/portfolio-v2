57e89cda8456c9bef3fbaa482484bca89168332b
krishnaanaril <krishnamohan.a.m@gmail.com>
2022-12-18 12:43:30 +0530

    feat: update webp converter and banner images

---
+++ build-scripts/convert-images.mjs
@@ -0,0 +1,23 @@
+import imagemin from 'imagemin';
+import webp from 'imagemin-webp';    // imagemin's WebP plugin.
+import fs from 'fs';
+
+const 
+    outputFolder = "./public/banners",            // Output folder
+    images = './public/banners/*.{jpeg,jpg,png}';        // PNG & JPEG images
+
+
+
+(async () => {
+    if(fs.existsSync(outputFolder)) {
+        await imagemin([images], {
+            destination: outputFolder,
+            plugins: [
+                webp({ quality: 50 })
+            ]
+        });
+        console.log('Images optimized.');
+    } else {
+        console.log('Output folder not found');
+    }    
+})();
