1bf3cbb2a691e30925d2b12f25b2c7827f123d30
krishnaanaril <krishnamohan.a.m@gmail.com>
2022-12-28 18:20:41 +0530

    feat: change image format to webp

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -5,7 +5,7 @@ export default function BlogCard({ blogData }: any) {
     const { id, title, description, image } = blogData;
     return (
         <div className="m-2 rounded-lg shadow-lg w-full py-5 md:w-2/5 lg:w-1/3 xl:w-1/4">
-            <Image src={`/${image}.png`} alt={title} className="w-full rounded-lg" quality="100" width={400} height={400} />
+            <Image src={`/${image}.webp`} alt={title} className="w-full rounded-lg" quality="100" width={400} height={400} />
             <div className="px-6 py-4">
                 <div className="text-gray-700 mt-1.5 mb-1.5 line-clamp-2 dark:text-gray-400">
                     <p className='line-clamp-3 min-h-[4.5rem]'>
