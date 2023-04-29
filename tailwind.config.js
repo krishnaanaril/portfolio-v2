a1fee7f6faa0fb7b64ee95e5b810db1b519f61c9
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-04-29 22:30:53 +0530

    chore: remove lineclamp from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp')
+    require('@tailwindcss/typography'),    
   ],
 }
