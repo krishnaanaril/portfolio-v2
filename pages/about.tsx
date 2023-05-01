b53e459ce8ca5179b8dd8ba1f0a313adee6eb384
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-05-01 21:06:35 +0530

    feat: update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,20 @@
+import Image from 'next/image';
 
-export default function About(){
+export default function About() {
     return (
-        <h1>About</h1>
+        <article className='prose dark:prose-invert lg:prose-xl md:prose-lg max-w-screen-md mx-auto'>
+            {/* <div className="text-center">
+                <h1 className="my-4">I'm Krishna Mohan.</h1>
+            </div> */}
+            <div className='flex flex-col flex-wrap'>
+                <div className="flex justify-center">
+                    <Image src="/images/dp.webp" alt="Krishna Mohan's DP" className="m-auto w-64 h-64 rounded-full" quality="100" width={400} height={400} />
+                </div>
+                <div className="text-center">
+                    <h2>I'm a Senior Full-Stack Engineer & Microsoft Certified Azure Solutions Architect</h2>
+                    <p>Right now I'm working as an <strong>Associate Project Manager</strong> at EY in Kochi, Kerala. </p>
+                </div>
+            </div>
+        </article>
     );
 }
