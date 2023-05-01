d58b0cccca20a1ab31bcedeaf0028454508be5df
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-05-01 11:10:49 +0530

    feat: add placeholder for share block

---
+++ components/social-share.tsx
@@ -0,0 +1,11 @@
+export default function SocialShare() {
+    return (
+        <div className="prose dark:prose-invert lg:prose-xl md:prose-lg max-w-screen-md mx-auto mt-6 mb-4">
+            <span>Share:</span>
+            <span>Twitter</span>
+            <span>Facebook</span>
+            <span>Mail</span>
+            <span>Copy Link</span>
+        </div>
+    )
+}
